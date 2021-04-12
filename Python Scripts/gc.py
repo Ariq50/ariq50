d8e6951d0c7229e5f6252dd6d4f108209040541b
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2021-04-11 18:12:07 -0700

    Add files via upload

---
+++ Python Scripts/gc.py
@@ -0,0 +1,39 @@
+#Calculates the GC content of a protein
+
+gene = open("BRCA1_SP1.txt", "r")
+
+#set the values to 0
+
+g = 0;
+c = 0;
+a = 0;
+t = 0;
+
+#skip the first line of header
+
+gene.readline()
+
+for line in gene:
+	line = line.lower()
+	for char in line:
+		if char == "g":
+			g += 1
+		if char == "a":
+			a += 1
+		if char == "c":
+			c += 1
+		if char == "t":
+			t += 1
+
+print ("number of g's:" + str(g))
+print ("number of c's:" + str(c))
+print ("number of a's:" + str(a))
+print ("number of t's:" + str(t))
+
+gc = (g + c + 0.) / (a + t + g + c + 0.)
+
+print ("gc content:" + str(gc))
+
+
+
+
