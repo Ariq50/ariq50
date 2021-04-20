2f3717602127a67e6eb27d4e89db2e2d34b2cdf1
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2021-04-20 03:26:14 -0700

    Update dna_into_rna.py

--- Python Scripts/dna_into_rna.py
+++ Python Scripts/dna_into_rna.py
@@ -1,3 +1,5 @@
+#Transcribing DNA into RNA
+
 #The following script transcribes the base DNA strand into RNA, using the replace function of Python
 
 #to replace the T's (thymine's) with U's (uracil's). The result is then printed to the console.
