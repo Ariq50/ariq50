140fa4d1fb217c694ab4a6b3041c2c727478ee6d
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2021-04-22 02:35:15 -0700

    Update pointMutationCounting.py

--- Python Scripts/pointMutationCounting.py
+++ Python Scripts/pointMutationCounting.py
@@ -1,3 +1,8 @@
+#Counting Point Mutations
+
+#The following script takes input from the user, in the form of two strings representing two different DNA strands.
+#A function is then used to iterate through both strings and compare the nucleotide bases in them. If the two characters being compared
+#do not match, the counter is incremented and the result printed to the console.
 
 string_1 = input("Please enter a DNA strand:")
 
@@ -6,7 +11,7 @@ string_2 = input("Please enter another DNA strand:")
 string_1_upper = string_1.upper()
 
 string_2_upper = string_2.upper()
-
+#function for comparing two strings and counting number of differing characters (Hamming Distance):
 def p_m_loop(string_1, string_2):
 	p_mutation = 0
 	for position in range(len(string_1)):
