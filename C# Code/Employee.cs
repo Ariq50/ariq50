2ec64fd43cdb4cfbf98ed84d96555e94c2461ca2
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2021-07-10 00:43:51 -0700

    Add files via upload

---
+++ C# Code/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOQTesting.BusinessLogic
+{
+    public class Employee
+    {
+        public string EmpName { get; set; }
+
+        private INotify notify;
+
+        public Employee(INotify _notify)
+        {
+            this.notify = _notify;
+        }
+
+        public void PayAndNotifyEmployee(Employee emp)
+        {
+            // some code
+            notify.SendMessage(emp);
+        }
+    }
+}
---
+++ C# Code/INotify.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOQTesting.BusinessLogic
+{
+    public interface INotify
+    {
+        bool SendMessage();
+
+        string SendMessage(string empName);
+
+        int SendMessage(int empId);
+
+        string SendMessage(int empId, string empName);
+
+        void SendMessage(Employee emp);
+
+        bool SendMessage(List<Employee> emps);
+    }
+}
