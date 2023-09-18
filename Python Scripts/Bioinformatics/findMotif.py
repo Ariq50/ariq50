f6459bfc7c6c4e29a9a5dde3f17460a72a9dc148
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-09-18 06:44:07 -0700

    Update findMotif.py

--- Python Scripts/Bioinformatics/findMotif.py
+++ Python Scripts/Bioinformatics/findMotif.py
@@ -1,6 +1,6 @@
-dnaStrand='GATATATGCATATACTT'
+dnaStrand=input('Please enter a DNA strand (no longer than 1000 nt in length):')
 
-motif='ATAT'
+motif=input('Please enter a motif to search for in the DNA strand:')
 
 for position in range(len(dnaStrand)):
     if dnaStrand[position:].startswith(motif):
