290b8f21b94174b656cca240b11f224852094e53
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-09-18 07:26:28 -0700

    Update ntCounter.py

--- Python Scripts/Bioinformatics/ntCounter.py
+++ Python Scripts/Bioinformatics/ntCounter.py
@@ -1,23 +1,27 @@
-# Script that calculates the number of times each nucleotide (A, C, G, T) is present in a given DNA strand.
-
 dnaStrand = input("Please enter a DNA strand (no longer than 1000 nt) whose nucleotide count (# of A's, C's, etc.) you would like to know: \n")
+
+valid = 'ACTG'
 
 def nt_loop(dnaStrand):
     nt_A = 0
     nt_C = 0
     nt_G = 0
     nt_T = 0
-	
+
     for position in range(len(dnaStrand)):
         if dnaStrand[position] == "A":
             nt_A += 1
         elif dnaStrand[position] == "C":
             nt_C += 1
         elif dnaStrand[position] == "G":
-            nt_G += 1
+            nt_G += 1 
         elif dnaStrand[position] == "T":
-            nt_T += 1
+            nt_T += 1  
         nt_num = [nt_A, nt_C, nt_G, nt_T]
     return nt_num
 
-print(*nt_loop(dnaStrand))
+for letter in dnaStrand:
+    if(len(dnaStrand) <= 1000 and letter in dnaStrand in valid):
+        print(*nt_loop(dnaStrand))
+else:
+    print("Please enter a valid DNA strand (contains A, C, T, G) with less than 1000 nt")
