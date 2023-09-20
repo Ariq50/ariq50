1b09267c6e90c64fea0c18c92857ec21692530b5
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-09-20 03:29:47 -0700

    Add files via upload

---
+++ Python Scripts/Bioinformatics/calcProteinMass.py
@@ -0,0 +1,51 @@
+protein = input("Please enter a protein whose mass you want to determine: ")
+
+def calcProteinMass(protein):
+    massProtein = 0
+
+    for i in range(0, len(protein)):
+            if protein[i] == 'A':
+                massProtein += 71.03711
+            elif protein[i] == 'C':
+                massProtein += 103.00919
+            elif protein[i] == 'D':
+                massProtein += 115.02694
+            elif protein[i] == 'E':
+                massProtein += 129.04259
+            elif protein[i] == 'F':
+                massProtein += 147.06841
+            elif protein[i] == 'G':
+                massProtein += 57.02146
+            elif protein[i] == 'H':
+                massProtein += 137.05891
+            elif protein[i] == 'I':
+                massProtein += 113.08406
+            elif protein[i] == 'K':
+                massProtein += 128.09496
+            elif protein[i] == 'L':
+                massProtein += 113.08406
+            elif protein[i] == 'M':
+                massProtein += 131.04049
+            elif protein[i] == 'N':
+                massProtein += 114.04293
+            elif protein[i] == 'P':
+                massProtein += 97.05276
+            elif protein[i] == 'Q':
+                massProtein += 128.05858
+            elif protein[i] == 'R':
+                massProtein += 156.10111
+            elif protein[i] == 'S':
+                massProtein += 87.03203
+            elif protein[i] == 'T':
+                massProtein += 101.04768
+            elif protein[i] == 'V':
+                massProtein += 99.06841
+            elif protein[i] == 'W':
+                massProtein += 186.07931
+            elif protein[i] == 'Y':
+                massProtein += 163.06333
+            else:
+                print("Please enter a valid protein.")
+    return(massProtein)
+
+print("The protein's mass is: " + "%3.3f" % calcProteinMass(protein))
