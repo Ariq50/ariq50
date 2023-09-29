a8af528c14332a01d124ca6c5dcf50bd946bba39
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-09-29 10:17:47 -0700

    Add files via upload

---
+++ C# Code/Bioinformatics/complementDNA.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace complementDNA
+{
+    class compDNA
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Please enter a DNA strand whose complement you would like to find: ");
+            
+            string userDNAStrand = Console.ReadLine();
+
+            compDNA c = new compDNA();
+
+            Console.WriteLine("The complement of the DNA strand you entered is:");
+            Console.WriteLine(c.complementDNAStrand(userDNAStrand));
+        }
+
+        public static string Reverse(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+
+        return reverse;
+        }
+
+        public string complementDNAStrand(string userDNAStrand)
+        {
+            string newDNAStrand = "";
+            int s = userDNAStrand.Length;
+
+            for (int i = 0; i < s; i++)
+            {
+                if (userDNAStrand[i] == 'A')
+                {
+                    newDNAStrand += 'T';
+                }
+                        
+                else if (userDNAStrand[i] == 'C')
+                {
+                     newDNAStrand += 'G';
+                }
+                       
+                else if (userDNAStrand[i] == 'T')
+                {
+                    newDNAStrand += 'A';
+                }
+                        
+                else if (userDNAStrand[i] == 'G')
+                {
+                     newDNAStrand += 'C';
+                }
+                       
+            }        
+        return compDNA.Reverse(newDNAStrand) ;
+        }
+    }
+}
