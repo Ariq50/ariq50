2781e24ce5406e718d24cb33c90aa5d53ffc7aa8
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-09-30 02:23:46 -0700

    Update findMotif.cs

--- C# Code/Bioinformatics/findMotif.cs
+++ C# Code/Bioinformatics/findMotif.cs
@@ -43,16 +43,4 @@ namespace findMotif
             }
         }
     }
-    public static class MyExtension
-    {
-      public static string ArrayToString(this Array arr)
-        {
-            List<object> lst = new List<object>();
-            object[] obj = new object[arr.Length];
-            Array.Copy(arr, obj, arr.Length);
-
-            lst.AddRange(obj);
-            return string.Join(",", lst.ToArray());
-        }
-    }
 }
