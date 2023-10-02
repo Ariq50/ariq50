82d4575d61ec7d6ea404d9cbe0e6b68f483f25c5
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-10-02 13:01:37 -0700

    Update gc2.py

--- Python Scripts/Bioinformatics/gc2.py
+++ Python Scripts/Bioinformatics/gc2.py
@@ -1,33 +1,36 @@
 #Calculates the melting temperature of a protein
 
-gene = open("BRCA1_SP1.txt", "r")
+import os
 
-#set the values to 0
+file_path = input('Enter the file path for the FASTA file of the protein whose GC content you wish to determine: ');
 
-g = 0;
-c = 0;
-a = 0;
-t = 0;
+if(os.path.exists(file_path):
+	gene = open(file_path, "r")
+	#set the values to 0
+	g = 0;
+	c = 0;
+	a = 0;
+	t = 0;
+	#skip the first line of header
+	gene.readline()
 
-#skip the first line of header
-
-gene.readline()
-
-for line in gene:
-	line = line.lower()
-	for char in line:
-		if char == g:
-			g += 1
-		if char == a:
-			a += 1
-		if char == c:
-			c += 1
-		if char == t:
-			t += 1
-
-tm = (2 * (a + t) + .0) + (4 * (g + c) + .0)
-
-print ("melting temperature:" + str(tm))
+	for line in gene:
+		line = line.lower()
+		for char in line:
+			if char == g:
+				g += 1
+			if char == a:
+				a += 1
+			if char == c:
+				c += 1
+			if char == t:
+				t += 1
+	
+	tm = (2 * (a + t) + .0) + (4 * (g + c) + .0)
+	
+	print ("melting temperature:" + str(tm))
+else:
+	print("Please enter a valid file path.")
 
 
 
