4d77f7f564b35605186d1432b83223b319d52040
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-10-23 03:50:49 -0700

    Add files via upload

---
+++ C# Code/Bioinformatics/rna_into_prot.cs
@@ -0,0 +1,177 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace rnaIntoProtein
+{
+    class rnaIntoProt
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the RNA strand you would like to convert into a Protein:");
+
+            string rna = Console.ReadLine();
+
+            string strand = rna.ToUpper();
+
+           string protein = "";
+           
+           for (var i = 0; i < strand.Length; i += 3)
+            {
+                 var codon = strand.Substring(i, Math.Min(3, strand.Length - i));
+                 switch (codon)
+                {
+                    case "GCU": 
+                    case "GCC": 
+                    case "GCA": 
+                    case "GCG": 
+                    
+                    protein += "A";
+                    break;
+
+                    case "UGU":
+                    case "UGC":
+                    
+                    protein += "C";
+                    break;
+
+                    case "GAU": 
+                    case "GAC": 
+
+                    protein += "D";
+                    break;
+
+                    case "GAA": 
+                    case "GAG":
+
+                    protein += "E";
+                    break;
+
+                    case "UUU":
+                    case "UUC":
+
+                    protein += "F";
+                    break;
+
+                    case "GGU": 
+                    case "GGC": 
+                    case "GGA": 
+                    case "GGG":
+
+                    protein += "G";
+                    break;
+
+                    case "CAU": 
+                    case "CAC":
+
+                    protein += "H";
+                    break;
+
+                    case "AUA": 
+                    case "AUU": 
+                    case "AUC":
+
+                    protein += "I";
+                    break;
+
+                    case "AAA": 
+                    case "AAG":
+
+                    protein += "K";
+                    break;
+
+                    case "UUA":
+                    case "UUG":
+                    case "CUU":
+                    case "CUC":
+                    case "CUA":
+                    case "CUG":
+
+                    protein += "L";
+                    break;
+
+                    case "AUG":
+                    
+                    protein += "M";
+                    break;
+
+                    case "AAU": 
+                    case "AAC":
+
+                    protein += "N";
+                    break;
+
+                    case "CCU": 
+                    case "CCC": 
+                    case "CCA": 
+                    case "CCG":
+
+                    protein += "P";
+                    break;
+
+                    case "CAA": 
+                    case "CAG":
+
+                    protein +="Q";
+                    break;
+
+                    case "CGU":
+                    case "CGC":
+                    case "CGA":
+                    case "CGG":
+                    case "AGA":
+                    case "AGG": 
+                    
+                    protein += "R";
+                    break;
+
+                    case "UCU":
+                    case "UCC":
+                    case "UCA":
+                    case "UCG":
+                    case "AGU":
+                    case "AGC":
+
+                    protein += "S";
+                    break;
+
+                    case "ACU": 
+                    case "ACC": 
+                    case "ACA": 
+                    case "ACG":
+
+                    protein += "T";
+                    break;
+
+                    case "GUU": 
+                    case "GUC": 
+                    case "GUA": 
+                    case "GUG":
+
+                    protein += "V";
+                    break;
+
+                    case "UGG":
+
+                    protein += "W";
+                    break;
+                    
+                    case "UAU":
+                    case "UAC":
+
+                    protein += "Y";
+                    break;
+                    
+                    case "UAA": 
+                    case "UAG": 
+                    case "UGA":
+                    
+                    protein += "*";
+                    break;
+                }
+            }
+            Console.WriteLine(protein);
+        }
+    }
+}
