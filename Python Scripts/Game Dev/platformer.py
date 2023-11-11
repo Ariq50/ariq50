cef407395c594af03ae207edfcc579eaa3cdc0fd
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-11-10 21:24:19 -0800

    Update platformer.py

--- Python Scripts/Game Dev/platformer.py
+++ Python Scripts/Game Dev/platformer.py
@@ -25,7 +25,7 @@ font_score = pygame.font.SysFont('Bauhaus 93', 30)
 tile_size = 25
 game_over = 0
 main_menu = True
-level = 0
+level = 3
 max_levels = 7
 score = 0
 
@@ -255,6 +255,12 @@ class World():
                 if tile == 3:
                     blob = Enemy(col_count * tile_size, row_count * tile_size - 10)
                     blob_group.add(blob)
+                if tile == 4:
+                    platform = Platform(col_count * tile_size, row_count * tile_size, 1, 0)
+                    platform_group.add(platform)
+                if tile == 5:
+                    platform = Platform(col_count * tile_size, row_count * tile_size, 0, 1)
+                    platform_group.add(platform)
                 if tile == 6:
                     lava = Lava(col_count * tile_size, row_count * tile_size + 13)
                     lava_group.add(lava)
@@ -284,7 +290,28 @@ class Enemy(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += self.move_direction
         self.move_counter += 1
-        if abs(self.move_counter) > 10:
+        if abs(self.move_counter) > 20:
+            self.move_direction *= -1
+            self.move_counter *= -1
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, move_x, move_y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('C:/Users/kian_/Downloads/PyGame/images/platform.png')
+        self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.move_counter = 0
+        self.move_direction = 1
+        self.move_x = move_x
+        self.move_y = move_y
+
+    def update(self):
+        self.rect.x += self.move_direction * self.move_x
+        self.rect.y += self.move_direction * self.move_y
+        self.move_counter += 1
+        if abs(self.move_counter) > 50:
             self.move_direction *= -1
             self.move_counter *= -1
 
@@ -319,6 +346,7 @@ class Exit(pygame.sprite.Sprite):
 player = Player(100, screen_height - 130)
 
 blob_group = pygame.sprite.Group()
+platform_group = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
 coin_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
@@ -354,6 +382,7 @@ while run:
 
         if game_over == 0:
             blob_group.update()
+            platform_group.update()
             #update score
             #check if a coin has been collected
             if pygame.sprite.spritecollide(player, coin_group, True):
@@ -362,6 +391,7 @@ while run:
             draw_text('X ' + str(score), font_score, white, tile_size - 10, 10)
         
         blob_group.draw(screen)
+        platform_group.draw(screen)
         lava_group.draw(screen)
         coin_group.draw(screen)
         exit_group.draw(screen)
