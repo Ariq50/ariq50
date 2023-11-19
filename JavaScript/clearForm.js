6c74859108948b8449f4b6bc6d6738935ec202e4
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-11-18 23:12:19 -0800

    Create clearForm.js

---
+++ JavaScript/clearForm.js
@@ -0,0 +1,10 @@
+// A function to clear the form
+        function clearForm() {
+            // Clear the input box and the input boxes
+            $("#phone-number").val("");
+            $("#area-code").val("");
+            $("#first-three").val("");
+            $("#last-four").val("");
+            // Clear the error message
+            $("#error-message").text("");
+        }
