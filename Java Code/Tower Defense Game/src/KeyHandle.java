fb05b6b7d1201e7ce8ae1eb2e01e49f815f34793
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-12-12 20:33:30 -0800

    Update KeyHandle.java

--- Java Code/Tower Defense Game/src/KeyHandle.java
+++ Java Code/Tower Defense Game/src/KeyHandle.java
@@ -1,6 +1,6 @@
 import java.awt.event.*;
 import java.awt.*;
-
+//Handles mouse input not covered by other classes
 public class KeyHandle implements MouseMotionListener, MouseListener {
 
 	
@@ -9,7 +9,7 @@ public class KeyHandle implements MouseMotionListener, MouseListener {
 		
 	}
 
-	
+	//Handles 'mousePressed' events within bounds of the tower store in game world
 	public void mousePressed(MouseEvent e) {
 		Screen.store.click(e.getButton());
 	}
@@ -32,14 +32,14 @@ public class KeyHandle implements MouseMotionListener, MouseListener {
 		
 	}
 
-	
+	//Handles 'mouseDragged' events within bounds of the game world
 	public void mouseDragged(MouseEvent e) {
 		Screen.mse = new Point((e.getX()) + ((Frame.size.width - Screen.myWidth)/2), 
 				(e.getY()) - ((Frame.size.height - (Screen.myHeight)) - 
 						(Frame.size.width - Screen.myWidth)/2));
 	}
 
-	
+	//Handles mouseMoved events within bounds of the game world
 	public void mouseMoved(MouseEvent e) {
 		Screen.mse = new Point((e.getX()) - ((Frame.size.width - Screen.myWidth)/2), 
 				(e.getY()) - ((Frame.size.height - (Screen.myHeight)) - 
