a4adaae892d517283fa6727a66e694b62fa18b5d
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-12-12 20:35:37 -0800

    Update Room.java

--- Java Code/Tower Defense Game/src/Room.java
+++ Java Code/Tower Defense Game/src/Room.java
@@ -1,17 +1,18 @@
 import java.awt.*;
 
 public class Room {
+	//Class variables
 	public int worldWidth = 12;
 	public int worldHeight = 8;
 	public int blockSize = 52;
 	
 	public Block[][] block;
 	
-	public Room() {
+	public Room() { //Class constructor
 		define();
 	}
 	
-	public void define() {
+	public void define() { //Defines game world dimensions
 		block = new Block[worldHeight][worldWidth];
 		
 		for(int y = 0; y < block.length; y++) {
@@ -22,7 +23,7 @@ public class Room {
 		}
 	}
 	
-	public void physic() {
+	public void physic() { //Defines bounds of tower range and calls associated method
 		for(int y = 0; y < block.length; y++) {
 			for(int x = 0; x < block[0].length; x++) {
 				block[y][x].physic();
@@ -30,7 +31,7 @@ public class Room {
 		}
 	}
 	
-	public void draw(Graphics g) {
+	public void draw(Graphics g) { //Draws towers and calls their fight method
 		for(int y = 0; y < block.length; y++) {
 			for(int x = 0; x < block[0].length; x++) {
 				block[y][x].draw(g);
