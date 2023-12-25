9e409da2c17c7f3fce07e1461b6b94c3e49a3312
Ariq50 <76070573+Ariq50@users.noreply.github.com>
2023-12-25 03:17:41 -0800

    Create ReverseString.cs

---
+++ C# Code/LeetCode/ReverseString.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public void ReverseString(char[] s) 
+    {
+        Array.Reverse(s);
+
+    foreach (char reverse in s)
+    {
+      System.Console.Write(reverse + " ");
+    }
+    }
+}
